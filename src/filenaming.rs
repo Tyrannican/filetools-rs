ab327d5ad19a247eea085950f360366bca0bab31
Graham Keenan <graham.keenan@outlook.com>
2020-04-08 13:55:57 +0100

    Docs added

--- src/filenaming.rs
+++ src/filenaming.rs
@@ -1,3 +1,28 @@
+//! Functions that generate PathBuf filenames
+//! 
+//! # Examples
+//! 
+//! ```
+//! use std::path::PathBuf;
+//! use filetools::filenaming::FileNaming;
+//! 
+//! fn main() {
+//!     let custom_name = FileNaming::generate_name("test", ".pdf");
+//!     assert_eq!(custom_name, PathBuf::from("test.pdf"));
+//! 
+//!     // Name will be suffixed by the current time it was generated
+//!     let timestamped_name = FileNaming::generate_timestamped_name("test", ".pdf");
+//! 
+//!     // Random name is a UUIDv4 string suffixed by the extension
+//!     let random_name = FileNaming::generate_random_name(".pdf");
+//! 
+//!     // N-digit name is a number prefixed by X zeros
+//!     let n_digit_name = FileNaming::generate_n_digit_name(5, 4, ".pdf");
+//!     assert_eq!(n_digit_name, PathBuf::from("0005.pdf"));
+//! }
+//! ```
+//! 
+
 use std::path::PathBuf;
 use uuid::Uuid;
 use chrono::prelude::*;
@@ -5,6 +30,17 @@ use chrono::prelude::*;
 pub struct FileNaming;
 
 impl FileNaming {
+    /// Generates a `PathBuf` from a given and extension
+    /// 
+    /// Returns a `PathBuf` of the form `name.ext`
+    pub fn generate_name(name: &str, ext: &str) -> PathBuf {
+        PathBuf::from(format!("{}{}", name, ext))
+    }
+
+    /// Generates a `PathBuf` from a name and extention with a timestamp
+    /// If `f_name` is "", just uses the timestamp and extension
+    /// 
+    /// Returns `PathBuf` in the form `f_name_timestamp.ext`
     pub fn generate_timestamped_name(fname: &str, ext: &str) -> PathBuf {
         let dt = UTC::now().format("%d_%m_%Y_%Hh%Mm%Ss");
         
@@ -15,12 +51,18 @@ impl FileNaming {
         }
     }
 
+    /// Generates a random UUIDv4 `PathBuf`
+    /// 
+    /// Returns `PathBuf` in the form `uuid.ext`
     pub fn generate_random_name(ext: &str) -> PathBuf {
         let unique = Uuid::new_v4();
 
         PathBuf::from(format!("{}{}", unique.to_string(), ext))
     }
 
+    /// Generates a `PathBuf` from a `number` prefixed by `n_digits` zeros
+    /// 
+    /// Returns `PathBuf` of the form e.g `0005.ext`
     pub fn generate_n_digit_name(number: i32, n_digits: usize, ext: &str) -> PathBuf {
         PathBuf::from(format!("{:0fill$}{}", number, ext, fill=n_digits))
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,43 @@
+//! Simple functions to help with file naming and file iteration
+//! Ported from the [filetools](https://github.com/jgrizou/filetools) library written by Jonathan Grizou
+//! 
+//! # Examples
+//! 
+//! ```
+//! use filetools::filehelpers::FileHelpers;
+//! use std::io::Result;
+//! use std::path::PathBuf;
+//! 
+//! fn main() -> Result <()> {
+//!     /// Creating a directory
+//!     let new_path = PathBuf::from("./test");
+//!     let _ = FileHelpers::ensure_dir(new_path)?;
+//! 
+//!     /// Iterating through all files in a directory
+//!     let nr_search = PathBuf::from("./test");
+//!     let r_search = PathBuf::from("./test");
+//! 
+//!     // Non-recursive search of directroy, just files in search folder
+//!     let non_recursed_files = FileHelpers::list_files(nr_search, false);
+//! 
+//!     // Recursive search of directory, gets all files in directory and all sub-directories
+//!     let recursed_files = FileHelpers::list_files(r_search, true);
+//! 
+//!     /// Iterating through all folders in a directory
+//!     let nr_search = PathBuf::from("./test");
+//!     let r_search = PathBuf::from("./test");
+//! 
+//!     // Non-recursive search for all folders, just folders in search directory
+//!     let non_recursive_folders = FileHelpers::list_folders(nr_search, false);
+//! 
+//!     // Recursive search of all folders, all subfolders in a directory as well
+//!     let recursive_folders = FileHelpers::list_folders(r_search, true);
+//! 
+//!     Ok(())
+//! } 
+//! ```
+//! 
+
 pub mod filenaming;
 pub mod filehelpers;
 
@@ -10,14 +50,14 @@ mod tests {
     use std::path::PathBuf;
 
     #[test]
-    fn iterate_files_and_folders() -> Result<()>{
-        let files = FileHelpers::list_files(PathBuf::from("src"))?;
-        let folders = FileHelpers::list_folders(PathBuf::from("."))?;
+    fn iterate_files_and_folders() -> Result<()> {
+        let files = FileHelpers::list_files(PathBuf::from("src"), true)?;
+        let folders = FileHelpers::list_folders(PathBuf::from("."), false)?;
 
         // filehelpers.rs filenaming.rs lib.rs
         assert_eq!(files.len(), 3);
 
-        // target/ src/ .git/
+        // target/ src/ .git/ test/
         assert_eq!(folders.len(), 4);
         Ok(())
     }
